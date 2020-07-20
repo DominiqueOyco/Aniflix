81d4a2245cdb141015b7f90f846ed0c8aa65c6c2
DominiqueOyco <dominickoyco@gmail.com>
2020-07-19 20:09:31 -0700

    Update centralScript.js

--- centralScript.js
+++ centralScript.js
@@ -9,21 +9,52 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   var dots = document.getElementsByClassName("dot");
+//   if (n > slides.length) {slideIndex = 1}    
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 5000); // Change image every 5 seconds
+// }
+
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+
+  if (n > slides.length){
+    slideIndex = 1
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+
+  if (n < 1){
+    slideIndex = slides.length
   }
-  slides[slideIndex-1].style.display = "block";  
+
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slideIndex++;
+  if (slideIndex > slides.length){
+    slideIndex = 1
+  }
+
+  slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 7000); // Change image every 7 seconds
 }
 
 // function dayDisplay() {
