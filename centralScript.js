f04973e54c00319d43ff7151bcc2c35498005a02
DominiqueOyco <dominickoyco@gmail.com>
2020-07-25 19:06:54 -0700

    Update centralScript.js

--- centralScript.js
+++ centralScript.js
@@ -44,45 +44,6 @@ function showSlides(n) {
   timer = setTimeout(showSlides, 7000); // Change image every 5 seconds
 }
 
-
-// function dayDisplay() {
-//   var d = new Date();
-//   var weekday = new Array(7);
-//   weekday[0] = "Sunday";
-//   weekday[1] = "Monday";
-//   weekday[2] = "Tuesday";
-//   weekday[3] = "Wednesday";
-//   weekday[4] = "Thursday";
-//   weekday[5] = "Friday";
-//   weekday[6] = "Saturday";
-//   var sunday = ["8PM ONE PUNCH MAN", "8:30PM ROBIHACHI", "9PM UNSTOPPABLE HIGH KICK"];
-//   var monday = ["5PM POKEMON", "5:30PM ONE PIECE", "7:45PM GOLGO 13",  "8:15PM MONSTER", "8:45PM OUTLAW STAR", "9:15PM SPACE BROTHERS"];
-//   var tuesday = ["5PM POKEMON", "5:30PM ONE PIECE", "7:45PM GOLGO 13",  "8:15PM MONSTER", "8:45PM OUTLAW STAR", "9:15PM SPACE BROTHERS"];
-//   var wednesday = ["5PM POKEMON", "5:30PM ONE PIECE", "7:45PM GOLGO 13",  "8:15PM MONSTER", "8:45PM OUTLAW STAR", "9:15PM SPACE BROTHERS"];
-//   var thursday = ["5PM POKEMON", "5:30PM ONE PIECE", "7:45PM GOLGO 13",  "8:15PM MONSTER", "8:45PM OUTLAW STAR", "9:15PM SPACE BROTHERS"];
-//   var friday = ["5PM POKEMON", "5:30PM ONE PIECE", "7:45PM GOLGO 13",  "8:15PM MONSTER", "8:45PM OUTLAW STAR", "9:15PM SPACE BROTHERS"];
-//   var saturday = ["8PM HANDA KUN", "8:30PM D-FRAG", "9PM UNSTOPPABLE HIGH KICK"];
-
-//   var n = weekday[d.getDay()];
-//   document.getElementById("demo").innerHTML = n;
-
-//   if(n = 0){
-
-//   } else if (n = 1){
-
-//   } else if (n = 2){
-
-//   } else if (n = 3){
-
-//   } else if (n = 4){
-
-//   } else if (n = 5){
-
-//   } else if (n = 6){
-
-//   }
-// }
-
 //this section is dedicated to the schedule-home section of the homepage
 var today = new Date();
 var now = today.getDay();
@@ -99,17 +60,14 @@ today = ' ' + mm + '/' + dd + '/' + yyyy;
 document.getElementById('schedule-contents').innerHTML += "<strong>" + today + "<br> <br>";
 
 if(days[now] == "SUNDAY"){
-  //TODO: DISPLAY THE SUNDAY ARRAY
   for (var i=0; i < sunday.length; i++){
     document.getElementById('schedule-contents').innerHTML += sunday[i];
   }
 }else if(days[now] == "MONDAY" || days[now] == "TUESDAY" || days[now] == "WEDNESDAY" || days[now] == "THURSDAY" || days[now] == "FRIDAY"){
-  //TODO: DISPLAY THE WEEKDAY ARRAY
   for (var i=0; i < weekdays.length; i++){
     document.getElementById('schedule-contents').innerHTML += weekdays[i];
   }
 }else if(days[now] == "SATURDAY"){
-  //TODO: DISPLAY THE SATURDAY ARRAY
   for (var i=0; i < saturday.length; i++){
     document.getElementById('schedule-contents').innerHTML += saturday[i];
   }
