ff2e7ebd73dcdcbd9521e1d807fd16cc3bdfb27c
DominiqueOyco <dominickoyco@gmail.com>
2021-08-15 13:52:07 -0700

    SKET Update (2)

--- centralScript.js
+++ centralScript.js
@@ -55,13 +55,12 @@ var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a 
 %2CBottomLeft%2C0%2C35_SCLZZZZZZZ_.jpg' width='341' height='192'>9:10PM HIGH KICK THROUGH THE ROOF</a>\
 <a class='gallery-item'><img></a></div>"];
 
-var weekdays = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/pokemon.jpg' width='341' height='192'>6:30PM POKEMON</a>\
+var weekdays = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/pokemon.jpg' width='341' height='192'>6:45AM POKEMON</a>\
 <a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1319704/Latvian/1319704_Latvian\
-_ShowMasterKeyArt_2f12eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>7:00PM ONE PIECE</a>\
-<a class='gallery-item'><img src='https://occ-0-37-33.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABV6EposZsW5JDA8wc_L0K1eElqLvX5ewIWP0cZ34tTO2x7rUPS06wZYx7DFkS0SMknHSeTjAyn4wsTIKyPlhUbLaz8Y.jpg?r=559'\
- width='341' height='192'>7:30PM FULLMETAL ALCHEMIST BROTHERHOOD</a>\
-<a class='gallery-item'><img src='https://img3.hulu.com/user/v3/artwork/d516f13f-40b3-4232-9c4b-0624c2ea7133?base_image_bucket_\
-name=image_manager&base_image=0696c4e8-0f1f-415a-ae43-4b0200ccb674&size=600x338&format=jpeg' width='341' height='192'>8:00PM THE PRINCE OF TENNIS</a>\
+_ShowMasterKeyArt_2f12eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>7:15AM ONE PIECE</a>\
+<a class='gallery-item'><img src='https://img3.hulu.com/user/v3/artwork/ca21d947-e5b1-4c5d-b973-f638016b112c?base_image_bucket_name=image_manager&base_image=9ab1514f-b4b2-4c1c-8905-4268a709e12f&size=1200x630&format=jpeg'\
+ width='341' height='192'>7:30PM MOBILE SUIT GUNDAM WING</a>\
+<a class='gallery-item'><img src='images/sket-dance.jpg' width='341' height='192'>7:15PM SKET DANCE</a>\
 <a class='gallery-item'><img></a></div>"];
 
 var saturday = ["<br>", "<a class='gallery-item'><img src='https://2.bp.blogspot.com/-eTzJlZTxFxc/VvdmKhBkK8I/AAAAAAAAD2k/XCD1a0HfRvApC0hbbWlJnfg6DPKAH32ug/w1200-h630-p-k-no-nu/My%2BLawyer.jpg' width='341' height='192'>8:00PM MY LAWYER, MR.JO</a>\
