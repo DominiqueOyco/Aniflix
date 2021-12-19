4b671276526d593642da30679182c648548aaacf
DominiqueOyco <dominickoyco@gmail.com>
2021-12-19 10:36:05 -0800

    Updated Week 12/20-12/26

--- centralScript.js
+++ centralScript.js
@@ -49,12 +49,12 @@ var today = new Date();
 var now = today.getDay();
 var days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 
-var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg'\
- width='341' height='192'>8:00PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>8:30PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:00PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:30PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img></a></div>"];
+// var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg'\
+//  width='341' height='192'>8:00PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>8:30PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:00PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:30PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img></a></div>"];
 
 var weekdays = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/pokemon.jpg' width='341' height='192'>6:45AM POKEMON</a>\
 <a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1319704/Latvian/1319704_Latvian\
@@ -64,12 +64,12 @@ _ShowMasterKeyArt_2f12eb78-deff-e611-8175-020165574d09.jpg' width='341' height='
 <a class='gallery-item'><img src='https://img1.ak.crunchyroll.com/i/spire2/b3b313c3f65e9595ef69742d53cd622e1282761740_full.jpg' width='341' height='192'>7:15PM HAYATE THE COMBAT BUTLER</a>\
 <a class='gallery-item'><img></a></div>"];
 
-var saturday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg'\
- width='341' height='192'>8:00PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>8:30PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:00PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:30PM TADA NEVER FALLS IN LOVE</a>\
-<a class='gallery-item'><img></a></div>"];
+// var saturday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg'\
+//  width='341' height='192'>8:00PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>8:30PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:00PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img src='https://i.pinimg.com/originals/2e/ca/b0/2ecab022c987fd8611a055e50fad22e9.jpg' width='341' height='192'>9:30PM TADA NEVER FALLS IN LOVE</a>\
+// <a class='gallery-item'><img></a></div>"];
 
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
