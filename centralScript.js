23a9d8bf6a1a52a83c8f43bfe2658ccedfe8c98b
DominiqueOyco <dominickoyco@gmail.com>
2022-03-06 00:58:54 -0800

    Updated Week 3/7 - 3/13

--- centralScript.js
+++ centralScript.js
@@ -49,22 +49,18 @@ var today = new Date();
 var now = today.getDay();
 var days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"];
 
-var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://images-na.ssl-images-amazon.com/images/S/pv-target-images/7d180157b50c4264ad1f\
-a33deba3b1898f09ffa22ca26621b2f70b66f77aba68._UY500_UX667_RI_V_TTW_.jpg' width='341' height='192'>8:00PM PERSONA 3</a>\
+var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg'\
+ width='341' height='192'>8:00PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>8:30PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>9:00PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>9:30PM PRISON SCHOOL</a>\
 <a class='gallery-item'><img></a></div>"];
-
-// var sunday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/uzaki.jpg'\
-//  width='341' height='192'>8:00PM UZAKI CHAN WANTS TO HANG OUT</a>\
-// <a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>8:30PM UZAKI CHAN WANTS TO HANG OUT</a>\
-// <a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>9:00PM UZAKI CHAN WANTS TO HANG OUT</a>\
-// <a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>9:30PM UZAKI CHAN WANTS TO HANG OUT</a>\
-// <a class='gallery-item'><img></a></div>"];
 
 var weekdays = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/pokemon.jpg' width='341' height='192'>6:45AM POKEMON</a>\
 <a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1319704/Latvian/1319704_Latvian\
 _ShowMasterKeyArt_2f12eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>7:15AM ONE PIECE</a>\
-<a class='gallery-item'><img src='images/p4.jpg'\
- width='341' height='192'>6:45PM PERSONA 4: THE ANIMATION</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1291898/Latvian/1291898_Latvian_ShowMasterKeyArt_92190267-deff-e611-8175-020165574d09.jpg'\
+ width='341' height='192'>6:45PM NORAGAMI</a>\
 <a class='gallery-item'><img src='https://img1.ak.crunchyroll.com/i/spire2/b3b313c3f65e9595ef69742d53cd622e1282761740_full.jpg' width='341' height='192'>7:15PM HAYATE THE COMBAT BUTLER</a>\
 <a class='gallery-item'><img></a></div>"];
 
@@ -73,10 +69,10 @@ _ShowMasterKeyArt_2f12eb78-deff-e611-8175-020165574d09.jpg' width='341' height='
 // a33deba3b1898f09ffa22ca26621b2f70b66f77aba68._UY500_UX667_RI_V_TTW_.jpg' width='341' height='192'>8:00PM PERSONA 3</a>\
 // <a class='gallery-item'><img></a></div>"];
 
-var saturday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>8:00PM UZAKI CHAN WANTS TO HANG OUT 3</a>\
-<a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>8:30PM UZAKI CHAN WANTS TO HANG OUT</a>\
-<a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>9:00PM UZAKI CHAN WANTS TO HANG OUT</a>\
-<a class='gallery-item'><img src='images/uzaki.jpg' width='341' height='192'>9:30PM UZAKI CHAN WANTS TO HANG OUT</a>\
+var saturday = ["<br>", "<div class='gallery'><a class='gallery-item'><img></a><a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>8:00PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>8:30PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>9:00PM PRISON SCHOOL</a>\
+<a class='gallery-item'><img src='https://derf9v1xhwwx1.cloudfront.net/image/upload/oth/FunimationStoreFront/1301063/Latvian/1301063_Latvian_ShowMasterKeyArt_c212eb78-deff-e611-8175-020165574d09.jpg' width='341' height='192'>9:30PM PRISON SCHOOL</a>\
 <a class='gallery-item'><img></a></div>"];
 
 var dd = String(today.getDate()).padStart(2, '0');
